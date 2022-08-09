668a53a90dbe19c35e9abea10665f1d0790199ba
justatangman <jasontang127@gmail.com>
2022-08-09 22:35:44 +0000

    1 minute sleep, label with country attack count and total attack count

--- main.py
+++ main.py
@@ -9,46 +9,6 @@ import win32evtlog
 
 api_key = "c3b596ba6ab7401b9eef629eae5606ff"
 
-
-# print(response.status_code)
-# print(response.content)
-
-
-def date2sec(evt_date):
-    '''
-
-    This function converts dates with format
-
-    '12/23/99 15:54:09' to seconds since 1970.
-
-    '''
-
-    regexp = re.compile('(.*)\\s(.*)')  # store result in site
-
-    reg_result = regexp.search(evt_date)
-
-    date = reg_result.group(1)
-
-    the_time = reg_result.group(2)
-
-    date_list = date.split()
-
-    (mon, day, yr) = (date_list[0], date_list[1], date_list[2])
-
-    print(date)
-    print(the_time)
-
-    time_list = date_list[3].split(":")
-
-    (hr, min, sec) = (time_list[0], time_list[1], time_list[2])
-
-    tup = [yr, mon, day, hr, min, sec, 0, 0, 0]
-
-    sec = time.mktime(tup)
-
-    return sec
-
-
 ####Main program
 
 # initialize variables
@@ -163,66 +123,7 @@ while True:
     payloadJS.write("];")
     payloadJS.close()
 
-    print("Sleeping for 5 minutes")
-    time.sleep(300)
-    print("Slept for 5 minutes----------------------------------")
-# print
-# logtype, ' events found in the last 8 hours since:', begin_time
-#
-# try:
-#
-#     events = 1
-#
-#     while events:
-#
-#         events = win32evtlog.ReadEventLog(hand, flags, 0)
-#
-#         for ev_obj in events:
-#
-#             # check if the event is recent enough
-#
-#             # only want data from last 8hrs
-#
-#             the_time = ev_obj.TimeGenerated.Format()
-#
-#             # seconds = date2sec(the_time)
-#             #
-#             # if seconds < begin_sec - 28800: break
-#
-#             if ev_obj.EventID == 4625:
-#                 # data is recent enough, so print it out
-#
-#                 computer = str(ev_obj.ComputerName)
-#
-#                 cat = str(ev_obj.EventCategory)
-#
-#                 src = str(ev_obj.SourceName)
-#
-#                 record = str(ev_obj.RecordNumber)
-#
-#                 evt_id = str(winerror.HRESULT_CODE(ev_obj.EventID))
-#
-#                 evt_type = str(evt_dict[ev_obj.EventType])
-#
-#                 msg = str(win32evtlogutil.SafeFormatMessage(ev_obj, logtype))
-#
-#                 print(string.join((the_time, computer, src, cat, record, evt_id, evt_type, msg[0:15]), ':'))
-#
-#             # if seconds < begin_sec - 28800:
-#             #     break  # get out of while loop as well
-#
-#         win32evtlog.CloseEventLog(hand)
-# #
-# except:
-#     print("blah")
-#     print(traceback.print_exc(sys.exc_info()))
+    print("Sleeping for 1 minutes")
+    time.sleep(60)
+    print("Slept for 1 minutes----------------------------------")
 
-# var payload = [
-#         ['Country', 'Frequency', 'Area Percentage'],
-#         ['France',  1, 20],
-#         ['Germany', 2, 40],
-#         ['Poland',  3, 60],
-#         ['Canada', 4, 80],
-#         ['China', 5, 100],
-#         ['Burkina Faso', 5,100]
-#      ];
